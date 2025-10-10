eeaa405391cb9c0a81743fadd65d5417d8195c3a
Marcel Scherzinger <>
2025-10-10 20:45:04 +0200

    model: add macro for defining block attribute data types

---
+++ crates/model/src/blocks/_macros/mod.rs
@@ -0,0 +1,5 @@
+mod blocks;
+mod dt_impl;
+
+pub(crate) use blocks::define_blocks;
+pub(crate) use dt_impl::def_dt_impl;
--- crates/model/src/blocks/datatypes.rs
+++ crates/model/src/blocks/datatypes.rs
@@ -1,99 +1,21 @@
 use crate::{
     ArgumentReporterName, DropdownSelection, Expression, List, RefBlock, Variable,
-    blocks::dt_interface::{ValueAttrJsonElemtype, ValueAttributeFromJson},
     interpret_json::FormatError,
 };
 
-impl ValueAttrJsonElemtype for Variable {
-    const ELEMTYPE: &'static str = "variable";
-}
+use super::_macros::def_dt_impl;
+use crate::interpret_json as ij;
 
-impl ValueAttrJsonElemtype for Option<Variable> {
-    const ELEMTYPE: &'static str = "optional variable";
-}
+def_dt_impl!(
+    Variable: "variable" { ij::get_variable_ref },
+    DropdownSelection: "dropdown" { ij::get_dropdown_selection },
+    Expression: "expression" { ij::get_expression },
+    RefBlock: "blockref" { ij::get_block_ref },
+    List: "listref" { ij::get_list_ref },
+    ArgumentReporterName: "argumentreportername" { ij::get_argument_reporter_name },
 
-impl ValueAttrJsonElemtype for DropdownSelection {
-    const ELEMTYPE: &'static str = "dropdown";
-}
-
-impl ValueAttrJsonElemtype for Expression {
-    const ELEMTYPE: &'static str = "expression";
-}
-
-impl ValueAttrJsonElemtype for Option<Expression> {
-    const ELEMTYPE: &'static str = "optional expression";
-}
-
-impl ValueAttrJsonElemtype for RefBlock {
-    const ELEMTYPE: &'static str = "blockref";
-}
-
-impl ValueAttrJsonElemtype for Option<RefBlock> {
-    const ELEMTYPE: &'static str = "optional blockref";
-}
-
-impl ValueAttrJsonElemtype for List {
-    const ELEMTYPE: &'static str = "listref";
-}
-
-impl ValueAttrJsonElemtype for ArgumentReporterName {
-    const ELEMTYPE: &'static str = "argumentreportername";
-}
-
-impl ValueAttrJsonElemtype for Option<List> {
-    const ELEMTYPE: &'static str = "optional listref";
-}
-
-impl ValueAttributeFromJson for Variable {
-    fn value_from_json(value: &serde_json::Value) -> Result<Self, FormatError>
-    where
-        Self: Sized,
-    {
-        crate::interpret_json::get_variable_ref(value)
-    }
-}
-
-impl ValueAttributeFromJson for Expression {
-    fn value_from_json(value: &serde_json::Value) -> Result<Self, FormatError>
-    where
-        Self: Sized,
-    {
-        crate::interpret_json::get_expression(value)
-    }
-}
-
-impl ValueAttributeFromJson for RefBlock {
-    fn value_from_json(value: &serde_json::Value) -> Result<Self, FormatError>
-    where
-        Self: Sized,
-    {
-        crate::interpret_json::get_block_ref(value)
-    }
-}
-
-impl ValueAttributeFromJson for DropdownSelection {
-    fn value_from_json(value: &serde_json::Value) -> Result<Self, FormatError>
-    where
-        Self: Sized,
-    {
-        crate::interpret_json::get_dropdown_selection(value)
-    }
-}
-
-impl ValueAttributeFromJson for List {
-    fn value_from_json(value: &serde_json::Value) -> Result<Self, FormatError>
-    where
-        Self: Sized,
-    {
-        crate::interpret_json::get_list_ref(value)
-    }
-}
-
-impl ValueAttributeFromJson for ArgumentReporterName {
-    fn value_from_json(value: &serde_json::Value) -> Result<Self, FormatError>
-    where
-        Self: Sized,
-    {
-        crate::interpret_json::get_argument_reporter_name(value)
-    }
-}
+    Option<Variable>: "optional variable",
+    Option<Expression>: "optional expression",
+    Option<RefBlock>: "optional blockref",
+    Option<List>: "optional listref",
+);
