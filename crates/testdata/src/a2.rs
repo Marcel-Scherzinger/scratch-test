76cb000b25371090ac55c68a221e8b8d88486a3d
Marcel Scherzinger <>
2025-11-02 16:54:08 +0100

    test-a2: enable random generation

--- crates/testdata/src/a2.rs
+++ crates/testdata/src/a2.rs
@@ -32,59 +32,62 @@ impl ExerciseTest for A2a {
 fn run_a_with(interp: &interpreter::InterpreterBuilder, tests: &mut CategoryTests) {
     use itertools::Itertools;
 
-    tests.add_test_case(interp.prepare(), |test_case, messages, out| {
-        let randoms = out.requested_randoms();
+    tests.add_test_case(
+        interp.prepare().enable_random_generation(),
+        |test_case, messages, out| {
+            let randoms = out.requested_randoms();
 
-        if randoms.used_count() % 2 == 1 {
-            messages.notify(ODD_NUMBER_OF_RANDOMS);
-        }
-        use model::ScratchExpr;
-        if randoms
-            .iter_used()
-            .any(|num| !(1..=6).contains(&num.as_int()) || num.is_float())
-        {
-            messages.notify(RANDOMS_NOT_INT_1_TO_6);
-        }
+            if randoms.used_count() % 2 == 1 {
+                messages.notify(ODD_NUMBER_OF_RANDOMS);
+            }
+            use model::ScratchExpr;
+            if randoms
+                .iter_used()
+                .any(|num| !(1..=6).contains(&num.as_int()) || num.is_float())
+            {
+                messages.notify(RANDOMS_NOT_INT_1_TO_6);
+            }
 
-        let wuerfe = test_case.get_required_list("Würfe")?;
-        let haeufigkeiten = test_case.get_required_list("Häufigkeiten")?;
+            let wuerfe = test_case.get_required_list("Würfe")?;
+            let haeufigkeiten = test_case.get_required_list("Häufigkeiten")?;
 
-        let sums: Vec<model::SValue> = randoms
-            .iter_used()
-            .tuples()
-            .map(|(from, to)| model::SValue::Int(from.as_int() + to.as_int()))
-            .collect();
+            let sums: Vec<model::SValue> = randoms
+                .iter_used()
+                .tuples()
+                .map(|(from, to)| model::SValue::Int(from.as_int() + to.as_int()))
+                .collect();
 
-        let dist: HashMap<i64, usize> = sums.iter().map(|s| s.as_int()).counts();
-        let distribution = (1..=12)
-            .map(|index| dist.get(&index).cloned().unwrap_or(0) as i64)
-            .map(model::SValue::Int)
-            .collect_vec();
+            let dist: HashMap<i64, usize> = sums.iter().map(|s| s.as_int()).counts();
+            let distribution = (1..=12)
+                .map(|index| dist.get(&index).cloned().unwrap_or(0) as i64)
+                .map(model::SValue::Int)
+                .collect_vec();
 
-        if wuerfe
-            .iter()
-            .any(|v| !v.is_int() || !(1..=12).contains(&v.as_int()))
-        {
-            messages.notify(WUERFE_NOT_ONLY_1_TO_12);
-        }
-        let mut failed = false;
-        let haeufigkeiten = haeufigkeiten.to_vec();
+            if wuerfe
+                .iter()
+                .any(|v| !v.is_int() || !(1..=12).contains(&v.as_int()))
+            {
+                messages.notify(WUERFE_NOT_ONLY_1_TO_12);
+            }
+            let mut failed = false;
+            let haeufigkeiten = haeufigkeiten.to_vec();
 
-        if format!("{sums:?}") != format!("{wuerfe:?}") {
-            let wuerfe = wuerfe.to_vec();
-            test_case.set_list_comparison("Würfe", wuerfe, sums);
-            failed = true;
-        }
+            if format!("{sums:?}") != format!("{wuerfe:?}") {
+                let wuerfe = wuerfe.to_vec();
+                test_case.set_list_comparison("Würfe", wuerfe, sums);
+                failed = true;
+            }
 
-        if format!("{distribution:?}") != format!("{haeufigkeiten:?}") {
-            test_case.set_list_comparison("Häufigkeiten", haeufigkeiten, distribution);
-            failed = true;
-        }
+            if format!("{distribution:?}") != format!("{haeufigkeiten:?}") {
+                test_case.set_list_comparison("Häufigkeiten", haeufigkeiten, distribution);
+                failed = true;
+            }
 
-        let error = test_case.program_error();
-        if error.is_some() || failed {
-            Err(())?;
-        }
-        Ok(())
-    });
+            let error = test_case.program_error();
+            if error.is_some() || failed {
+                Err(())?;
+            }
+            Ok(())
+        },
+    );
 }
