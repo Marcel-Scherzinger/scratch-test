04044891a7c34e9b8fade793904e96f51af74399
Marcel Scherzinger <>
2025-11-07 11:38:14 +0100

    model: add sections to lib docs for project documents and targets

--- crates/model/src/lib.rs
+++ crates/model/src/lib.rs
@@ -48,6 +48,38 @@
 //!
 //! A [Scratch file](https://en.scratch-wiki.info/wiki/Scratch_File_Format#Project_Files)
 //! is a ZIP file with images, sounds and a `project.json`
+//!
+//! [`ProjectDoc`] offers associated functions for constructing a
+//! document from a ZIP file like [`ProjectDoc::from_sb3_file`].
+//! But if the used decompression algorithm is unable to read the file
+//! or if you want to provide the `project.json` file directly
+//! you can use [`ProjectDoc::from_json`] which can be called with
+//! an already parsed JSON document.
+//! (See [`ProjectDoc`] for details.)
+//!
+//! (The next sections will explain how all program blocks are
+//! extracted from an entered JSON document
+//! – regardless how it was obtained in this section.)
+//!
+//! ## Targets
+//!
+//! Each document contains some meta information and – most
+//! importantly – a `targets` field containing a list of "targets".
+//! A [target](https://en.scratch-wiki.info/wiki/Scratch_File_Format#Targets)
+//! is a sprite or the background and contains different data.
+//! (The sprites have _local_ variables and the background's
+//! variables and lists are seen as _global_.)
+//!
+//! The relevant information stored in the modeled target:
+//! - [`TargetVariables`]: all variables of the target (local for sprites and global for the stage)
+//! - [`TargetLists`]: all lists of the target (local for sprites and global for the stage)
+//! - [`TargetBlocks`]: collection of all statement, expression, comparison, event, ... blocks of the program
+//! - [`TargetProcedures`]: generated from the blocks.
+//!   (The information about a procedure is split
+//!   across multiple blocks in a weird format.
+//!   This combines this data per procedure)
+//!
+//! (See [`Target`] for details.)
 
 /// copied from [<https://github.com/scratchfoundation/scratch-vm/blob/develop/src/serialization/sb3.js>]
 pub mod constants;
